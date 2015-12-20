4922feae940bd7a23916101cbf0a9c84e00fb4f7
zhyuanyuan <zhyyituse@163.com>
2015-12-20 18:12:24 +0800

    加了Grunt

--- App/scripts/app.js
+++ App/scripts/app.js
@@ -2,14 +2,24 @@
  * Created by zhyuanyuan on 2015/10/27.
  */
 
-//换肤接口
-var cssStyle = {};
-cssStyle.jqueryUi = "css!../framworks/jquery_ui/jquery-ui.css";
-cssStyle.bootStrap = "css!../framworks/bootstrap_3/css/bootstrap.min.css";
-cssStyle.ztree="css!../framworks/ztree/zTreeStyle.css";
-cssStyle.jqGrid="css!../framworks/jqGrid/ui.jqgrid.css";
+/*
+define("cssStyle", [], function () {
+
+    //换肤接口
+    var cssStyle = {};
+    cssStyle.jqueryUi = "css!../framworks/jquery_ui/jquery-ui.css";
+    cssStyle.bootStrap = "css!../framworks/bootstrap_3/css/bootstrap.min.css";
+    cssStyle.ztree = "css!../framworks/ztree/zTreeStyle.css";
+    cssStyle.jqGrid = "css!../framworks/jqGrid/ui.jqgrid.css";
+    return {
+        cssStyle: cssStyle
+    }
+
+});
+*/
 
 
+/*
 require.config({
     baseUrl: "./scripts",
     paths: {
@@ -23,11 +33,11 @@ require.config({
         "jquery-validate": "../framworks/jquery.validate",
         "mock": "../framworks/mock-min",
         "mock-module": "config/common/mockModule",
-        "ztree":"../framworks/ztree/jquery.ztree.all-3.5.min",
-        "jqGrid-cn":"../framworks/jqGrid/i18n/grid.locale-cn",
-        "grid.base":"../framworks/jqGrid/grid.base",
-        "jqGrid":"../framworks/jqGrid/jquery.jqGrid",
-        "angular-aminate":"../framworks/angular1_3/angular-animate"
+        "ztree": "../framworks/ztree/jquery.ztree.all-3.5.min",
+        "jqGrid-cn": "../framworks/jqGrid/i18n/grid.locale-cn",
+        "grid.base": "../framworks/jqGrid/grid.base",
+        "jqGrid": "../framworks/jqGrid/jquery.jqGrid",
+        "angular-aminate": "../framworks/angular1_3/angular-animate"
     },
     waitSeconds: 15,
     map: {
@@ -58,17 +68,79 @@ require.config({
         },
         "jquery-validate": ["jquery"],
         "mock-module": ["mock"],
-        "ztree":{
-            deps:["jquery",cssStyle.ztree],
-            exports:"ztree"
+        "ztree": {
+            deps: ["jquery", cssStyle.ztree],
+            exports: "ztree"
         },
-        "jqGrid":{
-            deps:["jquery","jquery-ui","jqGrid-cn",cssStyle.jqGrid],
-            exports:"jqGrid"
+        "jqGrid": {
+            deps: ["jquery", "jquery-ui", "jqGrid-cn", cssStyle.jqGrid],
+            exports: "jqGrid"
         },
-        "angular-aminate":["jquery","angular"]
+        "angular-aminate": ["jquery", "angular"]
     }
 });
+*/
+
+require.config({
+    baseUrl: "./scripts",
+    paths: {
+        "jquery": "../framworks/jquery",
+        "angular": "../framworks/angular1_3/angular",
+        "angular-ui-router": "../framworks/angular-ui-router",
+        "angular-resource": "../framworks/angular1_3/angular-resource",
+        "domReady": "../framworks/domReady",
+        "jquery-ui": "../framworks/jquery_ui/jquery-ui",
+        "bootstrap": "../framworks/bootstrap_3/js/bootstrap.min",
+        "jquery-validate": "../framworks/jquery.validate",
+        "mock": "../framworks/mock-min",
+        "mock-module": "config/common/mockModule",
+        "ztree": "../framworks/ztree/jquery.ztree.all-3.5.min",
+        "jqGrid-cn": "../framworks/jqGrid/i18n/grid.locale-cn",
+        "grid.base": "../framworks/jqGrid/grid.base",
+        "jqGrid": "../framworks/jqGrid/jquery.jqGrid",
+        "angular-aminate": "../framworks/angular1_3/angular-animate"
+    },
+    waitSeconds: 15,
+    map: {
+        "*": {
+            css: "../framworks/css.min"
+        }
+    },
+    shim: {
+        "angular": {
+            deps: ["jquery"],
+            exports: "angular"
+        },
+        "angular-ui-router": {
+            deps: ["angular"],
+            exports: "angular-ui-router"
+        },
+        "angular-resource": {
+            deps: ["angular"],
+            exports: "angular-resource"
+        },
+        "jquery-ui": {
+            deps: ["jquery","css!../framworks/jquery_ui/jquery-ui.css"],
+            exports: "jquery-ui"
+        },
+        "bootstrap": {
+            deps: ["jquery","css!../framworks/bootstrap_3/css/bootstrap.min.css"],
+            exports: "bootstrap"
+        },
+        "jquery-validate": ["jquery"],
+        "mock-module": ["mock"],
+        "ztree": {
+            deps: ["jquery", "css!../framworks/ztree/zTreeStyle.css"],
+            exports: "ztree"
+        },
+        "jqGrid": {
+            deps: ["jquery", "jquery-ui", "jqGrid-cn", "css!../framworks/jqGrid/ui.jqgrid.css"],
+            exports: "jqGrid"
+        },
+        "angular-aminate": ["jquery", "angular"]
+    }
+});
+
 
 var frameConfig = {};
 
@@ -78,10 +150,13 @@ frameConfig.paths = ["angular", "angular-ui-router", "angular-resource"];
 //模板接口名称
 frameConfig.interfaceName = ["ui.router", "ngResource"];
 
+var mainConfig = ["angular", "main", "domReady"].concat(config.mainConfig),
+    moduleName = frameConfig.interfaceName.concat(config.moduleName),
+    modulePath = frameConfig.paths.concat(config.modulePath);
+
+
 require(["config/config"], function (config) {
-    var mainConfig = ["angular", "main", "domReady"].concat(config.mainConfig),
-        moduleName = frameConfig.interfaceName.concat(config.moduleName),
-        modulePath = frameConfig.paths.concat(config.modulePath);
+
 
     define("main", modulePath, function (angular) {
         return angular.module("Main", moduleName);
